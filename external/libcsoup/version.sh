dd2bd5911f655f6159cc24a6521c05ba2d796c1e
Andy Xuming <xuminic@gmail.com>
2024-08-29 13:28:00 +1000

    Removed the libcsoup and libiup source tree.
    They will be pulled from github when building rename express,
    and will be packed into release package so it can be built offline.

--- external/libcsoup/version.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-MAJOR=`grep LIBCSOUP_VER_MAJOR libcsoup.h | cut -f 2`
-MINOR=`grep LIBCSOUP_VER_MINOR libcsoup.h | cut -f 2`
-BUGS=`grep LIBCSOUP_VER_BUGFIX libcsoup.h | cut -f 2`
-echo $MAJOR.$MINOR.$BUGS
